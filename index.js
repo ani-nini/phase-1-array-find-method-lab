c73fefcaaf138939a1eeede2e0c0a2ca53c89dd8
anissa guembre <guembreani@gmail.com>
2021-05-03 19:17:43 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin (array) {
+const result = array.find(array => (array.result === 'W'));
+if (result === undefined){
+    return undefined;
+}
+else{
+    return result.year;
+}
 
+    
+      
+    }
